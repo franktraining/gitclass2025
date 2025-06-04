b4052a78c2ab2a6046cbe11e2cf83584d0e3bb4a
franktraining <musabaytechcorp@gmail.com>
2025-06-03 21:19:43 -0400

    added one more welcome msg

--- gitexample/src/gitexample/Testone.java
+++ gitexample/src/gitexample/Testone.java
@@ -6,5 +6,6 @@ public class Testone {
 	public static void main(String[] args) {
 		System.out.println("hello github");
 		System.out.println("hello jon");
+		System.out.println("hello myself");
 	}
 }
