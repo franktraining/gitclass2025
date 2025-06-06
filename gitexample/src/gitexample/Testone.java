63c4a76a932186cb6842e9a305398b1cbf5f434f
zuleima <zuleimacruz91@yahoo.com>
2025-06-05 21:20:44 -0400

    I made a change

--- gitexample/src/gitexample/Testone.java
+++ gitexample/src/gitexample/Testone.java
@@ -4,7 +4,7 @@ public class Testone {
 	
 
 	public static void main(String[] args) {
-		System.out.println("hello github");
+		System.out.println("hello zuleimass");
 		System.out.println("hello jon");
 		System.out.println("hello myself");
 	}
