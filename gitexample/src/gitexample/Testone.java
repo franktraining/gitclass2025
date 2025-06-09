f06cc8dcaa03b027509879a2e407a5969e34121b
zuleima <zuleimacruz91@yahoo.com>
2025-06-09 14:03:36 -0400

    I made changes to your code

--- gitexample/src/gitexample/Testone.java
+++ gitexample/src/gitexample/Testone.java
@@ -4,10 +4,11 @@ public class Testone {
 	
 
 	public static void main(String[] args) {
-		System.out.println("hello github");
-		System.out.println("hello jon");
+		System.out.println("hello amy");
+		System.out.println("hello bob");
 		System.out.println("hello myself");
 		System.out.println("hello Java");
-
+        System.out.println("hello Java");
+		System.out.println("hello Java");
 	}
 }
